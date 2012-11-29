a16d4cbb5d4945f6fd05951b644d475aff8cece5
greyarch <gantcho@greyarch.net>
2012-11-29 18:13:09 +0100

    mongo - gone

--- app.js
+++ app.js
@@ -1,46 +1,8 @@
 var express = require("express");
-var mongoose = require("mongoose");
 var util = require("util");
 var app = express();
+var exams = require("./public/data/exams.json");
 
-if(process.env.VCAP_SERVICES){
-    var env = JSON.parse(process.env.VCAP_SERVICES);
-    var mongo = env['mongodb-1.8'][0]['credentials'];
-} else {
-    var mongo = {
-        "hostname":"localhost",
-        "port":27017,
-        "username":"",
-        "password":"",
-        "name":"",
-        "db":"db"
-    }
-}
-
-var generate_mongo_url = function(obj){
-    obj.hostname = (obj.hostname || 'localhost');
-    obj.port = (obj.port || 27017);
-    obj.db = (obj.db || 'test');
-    if(obj.username && obj.password){
-        return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
-    }
-    else{
-        return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
-    }
-}
-
-var mongourl = generate_mongo_url(mongo);
-var db = mongoose.createConnection(mongourl);
-
-var examSchema = new mongoose.Schema({
-    date: String,
-    month: String,
-    year: String,
-    title: String,
-    place: String   
-});
-
-var Exam = db.model('Exam', examSchema);
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
@@ -51,33 +13,19 @@ app.get('/', function(req, res) {
 });
 
 app.post('/exam', function(req, res) {
-    var x = new Exam(req.body);
     console.log("creating an exam: ");
-    console.dir(x);
-    x.save(function(err) {
-        if(err) {
-            console.log(err);
-            res.send(500, util.inspect(err));
-        } else res.end(200);
-    });
+    console.dir(req.body);
+    res.end();
 });
 
 app.get('/exam', function(req, res) {
-    Exam.find(function(err, exams) {
-        console.log("loading all exams");
-        res.json(exams);
-    });
+    console.log("loading all exams");
+    res.json(exams);
 });
 
 app.delete('/exam/:id', function(req, res) {
-    Exam.remove({
-        _id: req.params.id
-    }, function(err, exam) {
-        if(err) {
-            console.log(util.inspect(err));          
-            res.send(500, util.inspect(err));
-        } else res.send(200);
-    });
+    console.log("deleting an exam with an id " + req.params.id);
+    res.end();
 });
 
-app.listen(process.env.VCAP_APP_PORT || 3000);
+app.listen(3000);
