c0bda80108f6492d1de954f79210682501dd5fb2
Gantcho Kojuharov <gantcho@greyarch.net>
2012-12-01 21:21:23 +0100

    keep the exam selection on adding/deleting an exam

--- public/js/viewmodel.js
+++ public/js/viewmodel.js
@@ -5,23 +5,21 @@ function Exam(json) {
     self.title = ko.observable(json.title);
     self.id = ko.observable(json.id);
 
-    self.date = ko.computed(function() {
+    self.date = ko.computed(function () {
         return moment(json.date, "DD-MM-YYYY");
     });
 
-    self.day = ko.computed(function() {
+    self.day = ko.computed(function () {
         return self.date().date();
     });
 
-    self.month = ko.computed(function() {
+    self.month = ko.computed(function () {
         return self.date().month() + 1;
     });
 
-    self.year = ko.computed(function() {
+    self.year = ko.computed(function () {
         return self.date().year();
     });
-
-//    self.selected = ko.observable(false);
 }
 
 function AppViewModel() {
@@ -33,63 +31,69 @@ function AppViewModel() {
 
     self.availablePlaces = ko.observableArray(['Sofia, Bulgaria', 'Bucharest, Romania']);
 
-    deleteExam = function(exam) {
+    deleteExam = function (exam) {
+        console.log("deleting exam with id: ", exam.id());
         $.ajax({
-            url: "exam/" + exam.id(),
-            type: "DELETE",
-            success: function() {
+            url:"exam/" + exam.id(),
+            type:"DELETE",
+            success:function () {
                 self.loadAllExams();
             },
-            error: function(jqXhr) {
+            error:function (jqXhr) {
                 console.log("the error is: " + jqXhr.responseText);
             }
         });
     };
 
-    selectExam = function(exam) {
-        console.log("selected exam");
+    selectExam = function (exam) {
+        console.log("selected exam with id: ", exam.id());
         if (exam !== self.selectedExam()) {
             self.selectedExam(exam);
-            showParticipants(exam);
+            self.showParticipants(exam);
         }
     };
 
-    showParticipants = function(exam) {
+    self.showParticipants = function (exam) {
         console.log("showing exam participants");
         self.participants(exam.participants());
     };
 
-    self.loadAllExams = function() {
-        $.getJSON('exam', function(data) {
+    self.loadAllExams = function () {
+        self.participants([]);
+        $.getJSON('exam', function (data) {
             console.log("getting all exams");
-            self.exams($.map(data, function(item) {
-                return new Exam(item);
+            self.exams($.map(data, function (item) {
+                var newExam = new Exam(item);
+                if (self.selectedExam()) {
+                    if (self.selectedExam().id() === newExam.id()) {
+                        selectExam(newExam);
+                    }
+                }
+                return newExam;
             }));
-            self.participants([]);
         });
     };
 
-    newExam = function(title, place, date) {
+    newExam = function (title, place, date) {
         console.log("adding an exam");
         $.post('exam', {
-            date: date,
-            title: title,
-            place: place
-        }, function() {
+            date:date,
+            title:title,
+            place:place
+        }, function () {
             console.log("created the exam");
             self.loadAllExams();
         }, "json");
     };
 }
 
-$(function() {
-    $(".datepicker").click(function() {
+$(function () {
+    $(".datepicker").click(function () {
         $(".datepicker").glDatePicker({
-            zIndex: 100,
-            onChange: function(target, newDate)
-            {
+            zIndex:100,
+            onChange:function (target, newDate) {
                 target.val(
-                        newDate.getDate() + "/" +
+                    newDate.getDate() + "/" +
                         (newDate.getMonth() + 1) + "/" +
                         newDate.getFullYear());
             }
